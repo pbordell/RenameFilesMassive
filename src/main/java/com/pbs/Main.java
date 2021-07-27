da3914e61026cb88279799b0ad6c83a1d88a093e
PERE BORDELL SARDA <pere.bordellsarda@altran.com>
2021-07-27 22:57:04 +0200

    First implementation RenameFilesMassive

--- src/main/java/com/pbs/Main.java
+++ src/main/java/com/pbs/Main.java
@@ -1,51 +1,21 @@
 package com.pbs;
 
-import java.io.File;
-import java.util.Arrays;
-
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
-import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
-@SuppressWarnings({ "restriction" })
 public class Main extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-
-		final Label labelSelectedDirectory = new Label();
-
-		Button btnOpenDirectoryChooser = new Button();
-		btnOpenDirectoryChooser.setText("Carregui un directori amb arxius");
-		btnOpenDirectoryChooser.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				DirectoryChooser directoryChooser = new DirectoryChooser();
-				File selectedDirectory = directoryChooser.showDialog(stage);
-
-				if (selectedDirectory == null) {
-					labelSelectedDirectory.setText("No has seleccionat un directori!");
-				} else if (selectedDirectory.isDirectory() && selectedDirectory.list().length == 0) {
-					labelSelectedDirectory.setText("El directori seleccionat està buit!");
-				} else {
-					labelSelectedDirectory.setText(selectedDirectory.getAbsolutePath());
-				}
-			}
-		});
 
 		// Create the FXMLLoader
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/pbs/fxml/renamefilesmassive.fxml"));
 
 		// Create the Pane and all Details
 		VBox vBox = loader.load();
-		vBox.getChildren().addAll(0, Arrays.asList(labelSelectedDirectory, btnOpenDirectoryChooser));
 
 		// Create the Scene
 		Scene scene = new Scene(vBox);
--- src/main/java/com/pbs/controller/RenameFilesMassiveController.java
+++ src/main/java/com/pbs/controller/RenameFilesMassiveController.java
@@ -1,23 +1,33 @@
 package com.pbs.controller;
 
+import java.io.File;
 import java.net.URL;
-import java.nio.file.Path;
 import java.util.ResourceBundle;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
 
-import com.pbs.util.PropertiesUtils;
+import javax.swing.JOptionPane;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuItem;
-import javafx.stage.FileChooser;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
 
-@SuppressWarnings({ "restriction" })
 public class RenameFilesMassiveController implements Initializable {
 
 	@FXML
-	private Button change;
+	private TextField directoryTextField;
+
+	@FXML
+	private TextField inputTextToChange;
+
+	@FXML
+	private TextField inputTextChanged;
+
+	@FXML
+	private TextArea outputText;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -25,19 +35,70 @@ public class RenameFilesMassiveController implements Initializable {
 	}
 
 	@FXML
-	void changeAction(ActionEvent event) {
-		try {
-			FileChooser chooser = new FileChooser();
-			chooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Files", PropertiesUtils.readFormats()));
+	public void handleChangeDirectoryBtnClick() {
+		DirectoryChooser directoryChooser = new DirectoryChooser();
+		File selectedDirectory = directoryChooser.showDialog(null);
 
-			Path newFile = chooser.showOpenDialog(((MenuItem) event.getSource()).getParentPopup().getScene().getWindow()).toPath();
-			if (newFile != null) {
-				
-			}
-			
-		} catch (Exception e) {
-			e.printStackTrace();
+		if (selectedDirectory == null) {
+			directoryTextField.setText("No ha seleccionado ningún directorio!");
+		} else if (selectedDirectory.isDirectory() && selectedDirectory.list().length == 0) {
+			directoryTextField.setText("El directorio seleccionado está vacio!");
+		} else {
+			directoryTextField.setText(selectedDirectory.getAbsolutePath());
 		}
+	}
+
+	@FXML
+	void changeAction(ActionEvent event) {
+		if (validatefields()) {
+			try {
+				final String path = directoryTextField.getText();
+				final String oldName = inputTextToChange.getText();
+				final String newName = inputTextChanged.getText();
+
+				final StringBuilder result = new StringBuilder();
+				AtomicInteger runCount = new AtomicInteger(0);
+				Stream.of(new File(path).listFiles())
+						.filter(file -> !file.isDirectory())
+						.filter(file -> file.getName().contains(oldName))
+						.forEach(f -> {
+					String finalName = f.getName().replaceAll(oldName, newName);
+					String pathResultFile = f.getParent() + File.separator + finalName;
+					result.append("Cambio nombre del fichero " + f.getName() + " a " + finalName);
+					if(f.renameTo(new File(pathResultFile))) {
+						result.append(" OK \n");
+						runCount.getAndIncrement();
+					} else {
+						result.append(" KO \n");
+					}
+				});
+				
+				result.append("\nSe han modificado " + runCount.get() + " nombres de ficheros");
+				outputText.setText(result.toString());
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private boolean validatefields() {
+		StringBuilder sb = new StringBuilder();
+		if (directoryTextField.getText().equals("")) {
+			sb.append(" directoryTextField ");
+		}
+
+		if (inputTextToChange.getText().equals("")) {
+			sb.append(" inputTextToChange ");
+		}
+
+		String message = sb.toString();
+		if (message.length() > 0) {
+			JOptionPane.showMessageDialog(null, "Rellene los campos: " + message);
+			return false;
+		}
+
+		return true;
 	}
 
 }
